acacedfbd9759cb0f574d1296fc7eff161a654d3
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2019-11-17 21:45:03 +1030

    Feat: added connexion for swagger

--- flask/run.py
+++ flask/run.py
@@ -1,4 +1,17 @@
-from app import app
+from flask import redirect
+import connexion
+
+# Create the application instance
+app = connexion.FlaskApp(__name__, specification_dir='swagger/')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('api.yml')
+
+@app.route('/')
+@app.route('/api')
+def display_api_doc():
+    return redirect('/api/ui', code=302)
+
 
 if __name__ == '__main__':
     app.run()
